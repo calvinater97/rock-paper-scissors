59106d7fbf749a60566fcba809daa4b0238a9b3f
Calvin Wood <cjrakau2018@gmail.com>
2021-02-01 18:47:09 -0700

    started creating game

--- rock-paper-scissors/main.swift
+++ rock-paper-scissors/main.swift
@@ -7,6 +7,36 @@
 
 import Foundation
 
-print("Hello, World!")
+func displayStartGame() -> String {
+    return "This is the start of the greatest game of Rock Paper Scissors you gave ever played!!"
+}
 
-print("took me about 3 hours but i finally did it")
+func pickRandom() -> String {
+    let array = ["Rock", "Paper", "Scissors"]
+    let randomAction = array.randomElement()!
+    return randomAction
+}
+
+func playerChoice() -> String {
+    print("Please make your selection of Rock, Paper, or Scissors.")
+    let selection: String = readLine()!
+    return selection
+}
+
+func correctChoice() -> String {
+    print("Is this the correct action you would like to go with? (Y/N)")
+    let correct = readLine()!
+    return correct
+}
+let selection = playerChoice()
+let correct = correctChoice()
+let randomAction = pickRandom()
+let startGame = displayStartGame()
+
+print(startGame)
+
+while correct == "N" {
+    playerChoice()
+    correctChoice()
+}
+
