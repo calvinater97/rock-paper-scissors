b8d3028429a78abe38776b152b3e6a3f64be3a18
Calvin Wood <cjrakau2018@gmail.com>
2021-02-02 18:44:51 -0700

    finished game

--- rock-paper-scissors/main.swift
+++ rock-paper-scissors/main.swift
@@ -9,19 +9,19 @@ import Foundation
 
 //Function for printing the start of the game.
 func displayStartGame() -> String {
-    return "This is the start of the greatest game of Rock Paper Scissors you gave ever played!!"
+    return "This is the start of the greatest game of Rock Paper Scissors Lizard Spock you gave ever played!!"
 }
 
 //This function will pick a random element from the array provided.
 func pickRandom() -> String {
-    let array = ["Rock", "Paper", "Scissors"]
+    let array = ["Rock", "Paper", "Scissors", "Lizard", "Spock"]
     let randomAction = array.randomElement()!
     return randomAction
 }
 
 //This function prompts the user for input.
 func playerChoice() -> String {
-    print("Please make your selection of Rock, Paper, or Scissors.")
+    print("Please make your selection of Rock, Paper, Scissors, Lizard or Spock.")
     let selection: String = readLine()!
     return selection
 }
@@ -59,6 +59,12 @@ if selection == "Rock" && randomAction == "Paper" {
 if selection == "Rock" && randomAction == "Scissors" {
     print("You're a WINNER!!")
 }
+if selection == "Rock" && randomAction == "Lizard" {
+    print("You're a WINNER!!")
+}
+if selection == "Rock" && randomAction == "Spock" {
+    print("You're a loser")
+}
 
 //If you selected Paper this will determine your outcome.
 if selection == "Paper" && randomAction == "Paper" {
@@ -70,6 +76,12 @@ if selection == "Paper" && randomAction == "Rock" {
 if selection == "Paper" && randomAction == "Scissors" {
     print("You're a loser")
 }
+if selection == "Paper" && randomAction == "Lizard" {
+    print("You're a loser")
+}
+if selection == "Paper" && randomAction == "Spock" {
+    print("You're a WINNER!!")
+}
 
 //If you selected Scissors this will determine your outcome.
 if selection == "Scissors" && randomAction == "Scissors" {
@@ -79,6 +91,46 @@ if selection == "Scissors" && randomAction == "Paper" {
     print("You're a WINNER")
 }
 if selection == "Scissors" && randomAction == "Rock" {
+    print("You're a loser")
+}
+if selection == "Scissors" && randomAction == "Lizard" {
+    print("You're a WINNER")
+}
+if selection == "Scissors" && randomAction == "Spock" {
+    print("You're a loser")
+}
+
+//If you selected Lizard this will determine your outcome.
+if selection == "Lizard" && randomAction == "Lizard" {
+    print("Its a draw!!")
+}
+if selection == "Lizard" && randomAction == "Paper" {
+    print("You're a WINNER")
+}
+if selection == "Lizard" && randomAction == "Rock" {
+    print("You're a loser")
+}
+if selection == "Lizard" && randomAction == "Scissors" {
+    print("You're a loser")
+}
+if selection == "Lizard" && randomAction == "Spock" {
+    print("You're a WINNER")
+}
+
+//If you selected Spock this will determine your outcome.
+if selection == "Spock" && randomAction == "Spock" {
+    print("Its a draw!!")
+}
+if selection == "Spock" && randomAction == "Paper" {
+    print("You're a loser")
+}
+if selection == "Spock" && randomAction == "Rock" {
+    print("You're a WINNER")
+}
+if selection == "Spock" && randomAction == "Scissors" {
+    print("You're a WINNER")
+}
+if selection == "Spock" && randomAction == "Lizard" {
     print("You're a loser")
 }
 
